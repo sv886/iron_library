7851a2d6a86a1cb1b2261c8ca5d0e4861b0e39c0
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-03-05 13:00:49 -0600

    add author id migration

---
+++ db/migrate/20160305042331_add_column_author_id_to_books.rb
@@ -0,0 +1,5 @@
+class AddColumnAuthorIdToBooks < ActiveRecord::Migration
+  def change
+    add_column :books, :author_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303214816) do
+ActiveRecord::Schema.define(version: 20160305042331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
